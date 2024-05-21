12a5eb73c296edbbf629c07a55b3c6e755af22c3
ojaswagit <142043037+ojaswagit@users.noreply.github.com>
2024-05-21 10:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     # Read the contents of the HTML file
-    with open("portfolio.html", "r", encoding="utf-8") as f:
+    with open("mainportfolio.html", "r", encoding="utf-8") as f:
         html_content = f.read()
 
     # Embed the HTML content within the Streamlit app
